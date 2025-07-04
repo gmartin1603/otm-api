cb9ea4fd735f745184380753147bfbb091cbf704
George <duramaxer228@gmail.com>
2025-07-04 09:02:41 -0500

    Code clean up after successful staging release

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,3 @@
-// import * as functions from "firebase-functions";
 import { onRequest } from "firebase-functions/v2/https";
 import express, { Request, Response } from "express";
 // import { connectToMongoDB } from "./helpers/mongo";
@@ -13,110 +12,36 @@ import appService from "./services/app-service";
 const dotenv = require("dotenv");
 dotenv.config();
 
-// let env: string;
+let env: string;
 // let corsOrigin = "*";
-// if (process.env.NODE_ENV == "prod") {
-//   env = "prod";
-//   // corsOrigin = process.env.NODE_ENV_CORS_URL || "*";
-// } else {
-//   env = "dev";
-// }
+if (process.env.NODE_ENV == "prod") {
+  env = "prod";
+  // corsOrigin = process.env.NODE_ENV_CORS_URL || "*";
+} else {
+  env = "dev";
+}
 
-// console.log("CURRENT ENV: ", env);
-// const corsHandler = cors({ origin: true });
-
-// const activityLogger = async (req: Request, res: Response, next: () => any) => {
-//   try {
-//     const method = req.params[0] || "unknown";
-//     await _commonUtils.writeLog("activity", {
-//       message: `API call made to method: ${method}`,
-//       headers: req.headers,
-//       body: req.body,
-//     });
-//     return next();
-//   } catch (error) {
-//     console.error("Activity logging failed:", error);
-//     return next(); // Continue even if logging fails
-//   }
-// };
-
-// // To enable mongoContext, uncomment and add it to the middleware chain
-// // import { connectToMongoDB } from "./helpers/mongo";
-// // const mongoContext = (req: Request, res: Response, next: () => any) => {
-// //   connectToMongoDB((error) => {
-// //     if (error) {
-// //       console.error("Failed to connect to MongoDB", error);
-// //       return res.status(500).json({ error: "Failed to connect to MongoDB" });
-// //     }
-// //     next();
-// //   });
-// // };
-
-// function applyMiddleware(handler: (req: Request, res: Response) => any) {
-//   return (req: Request, res: Response) => {
-//     // return corsHandler(req, res, () => {
-//       // console.log("CORS Handler added");
-//       return bodyParser.json()(req, res, () => {
-//         // To enable mongoContext, add: mongoContext(req, res, () => { ... })
-//         return activityLogger(req, res, () => {
-//           return handler(req, res);
-//         });
-//       });
-//     // });
-//   };
-// };
-
-
-// const applyMiddleware = (handler: (req: Request, res: Response) => any) => (req, res) => {
-// 	return corsHandler(req, res, (_) => {
-//     console.log("CORS Handler added");
-// 		return bodyParser.json()(req, res, () => {
-//       // console.log("Adding MongoDB Middleware");
-// 			// connectToMongoDB((error) => {
-// 			// 	if (error) {
-// 			// 		console.error("Failed to connect to MongoDB", error);
-// 			// 		// return res.status(500).json({ error: "Failed to connect to MongoDB" });
-// 			// 	}
-//       const method = req.params[0] || "unknown";
-//       _commonUtils.writeLog("activity", {
-//         message: `API call made to method: ${method}`,
-//         headers: req.headers,
-//         body: req.body,
-//       }).then(() => {
-//         return handler(req, res);
-//       });
-// 			// });
-// 		});
-// 	});
-// };
-
-// **SCRIPT** CONTROLLER ROUTING START
+console.log("CURRENT ENV: ", env);
 
 const postingsApp = express();
 postingsApp.post("*", (req: Request, res: Response) => RequestHandler(req, res, postingsService));
 export const postings = onRequest({ cors: true }, postingsApp);
-// export const postings = functions.https.onRequest(applyMiddleware(postingsApp));
-
 
 const mainApp = express();
 mainApp.post("*", (req: Request, res: Response) => RequestHandler(req, res, appService));
 export const main = onRequest({ cors: true }, mainApp);
-// export const app = functions.https.onRequest(applyMiddleware(mainApp));
 
 const jobsApp = express();
 jobsApp.post("*", (req: Request, res: Response) => RequestHandler(req, res, jobsService));
 export const jobs = onRequest({ cors: true }, jobsApp);
-// export const jobs = functions.https.onRequest(applyMiddleware(jobsApp));
 
 const userApp = express();
 userApp.post("*", (req: Request, res: Response) => RequestHandler(req, res, userService));
 export const user = onRequest({ cors: true }, userApp);
-// export const user = functions.https.onRequest(applyMiddleware(userApp));
 
 const devAppExpress = express();
 devAppExpress.post("*", (req: Request, res: Response) => RequestHandler(req, res, devAppService));
 export const devApp = onRequest({ cors: true }, devAppExpress);
-// export const devApp = functions.https.onRequest(applyMiddleware(devAppExpress));
 
 // **SCRIPT** CONTROLLER ROUTING END
 
